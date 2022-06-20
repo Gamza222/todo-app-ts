25f02bbe1493f0c0c3c5235cfc3ce918fa71958d
Gamza222 <turpurture@mail.ru>
2022-06-20 12:56:13 +0300

    all done

---
+++ src/components/model/model.ts
@@ -0,0 +1,28 @@
+
+export interface Todo{
+    id: number;
+    todo: string;
+    isDone: boolean;  
+}
+
+{/*}
+type Actions = 
+| {type: 'add'; payload: string}
+| {type: 'remove'; payload: number}
+| {type: 'done'; payload: number};
+
+const TodoReducer = (state: Todo[], action) => {
+    
+};
+
+import { useReducer } from "react";
+
+export const ReducerExamplel = () => {
+
+    const [state, dispatch] = useReducer(TodoReducer, [])
+
+  return (
+    <div>model</div>
+  )
+}
+*/}
